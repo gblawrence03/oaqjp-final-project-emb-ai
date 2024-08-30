27fcca82b590a2c56f0a7b7a9e2e5ddd45952456
gblawrence03 <georgeblawrence@hotmail.co.uk>
2024-08-30 05:44:54 -0400

    format detector output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = ("https://sn-watson-emotion.labs.skills.network/v1/watson."
@@ -6,4 +7,7 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json =  { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers=header, json=input_json)
-    return response.text
+    response_formatted = json.loads(response.text)
+    emotions = response_formatted["emotionPredictions"][0]["emotion"]
+    emotions["dominant_emotion"] = max(emotions, key=lambda key: emotions[key])
+    return emotions
